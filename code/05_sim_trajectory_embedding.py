a14246242fff089a9917afbfa2de1bc0fc54190d
Jun Zhang <zhangjun310@live.com>
2017-08-21 17:53:01 +0800

    Add start end tag option
    Add start end tag option

---
+++ code/05_sim_trajectory_embedding.py
@@ -0,0 +1,56 @@
+import numpy as np
+import glob
+
+def cos_sim(X):
+    X_v = np.array(X)
+    dot = np.dot(X_v,X_v.transpose())
+    n = np.linalg.norm(X_v,axis=1)
+    nn = np.dot(n.reshape(-1,1),n.reshape(1,-1))
+    cos_similarity = dot/nn
+    return cos_similarity
+
+trajectory_embedding_matrix_path = glob.glob('../data/trajectory_embedding_matrix*')
+
+print 'we have those embedding_matrix '
+
+zip_path = zip(range(len(trajectory_embedding_matrix_path)),trajectory_embedding_matrix_path)
+
+for x in zip_path:
+    print x
+
+num = input('choose which one to use [Enter the num] : ')
+
+path = trajectory_embedding_matrix_path[num]
+
+desc = path.split('matrix')[1].split('.npy')[0]
+
+trajectory_embedding_matrix = np.load(path)
+
+def max_min_normalization(M):
+    return (M-M.min(1).reshape(-1,1)) / (M.max().reshape(-1,1) - M.min().reshape(-1,1))
+
+trajectory_embedding_matrix = max_min_normalization(trajectory_embedding_matrix)
+
+
+print 'calculate similarity matrix'
+trajectory_sim = cos_sim(trajectory_embedding_matrix)
+print 'sort similarity matrix'
+trajectory_sim_order_index = trajectory_sim.argsort()[:,::-1]
+
+
+trajectory_sim_result_path = '../data/trajectory_sim_result' + desc
+
+sim_filter_threshold = input('sim_filter_threshold : ') #0.9
+
+f = open(trajectory_sim_result_path,'w')
+
+for sim_order_index ,sim  in zip(trajectory_sim_order_index,trajectory_sim):
+    sim = sim[sim_order_index]
+    sim_order_index = sim_order_index[sim > sim_filter_threshold]
+    sim = [round(x,3) for x in sim[sim > sim_filter_threshold]]
+    zip_index_sim = zip(sim_order_index,sim)
+    line = str(sim_order_index[0]) + '\t' + '\t'.join(map(str,zip_index_sim[1:])) + '\n'
+    print line
+    f.write(line)
+f.close()
+
